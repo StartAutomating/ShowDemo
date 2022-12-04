4d840604a73111a61269b6241a647e4a418c6324
StartAutomating <StartAutomating@github.com>
2022-12-04 22:16:27 +0000

    Removing Get-Demo.ps.md

--- Get-Demo.ps1
+++ Get-Demo.ps1
@@ -34,8 +34,7 @@ function Get-Demo {
             return
         }
         $filePaths =
-            @(            
-            $pwd
+            @(
             if ($myModule) {
                 $moduleRelationships = 
                                        @(
@@ -83,12 +82,102 @@ function Get-Demo {
             }
             )
         $allDemoFiles =
+            @($(
+              # Collect all items into an input collection
+              $inputCollection =$(
+              $executionContext.SessionState.InvokeCommand.GetCommands('*','Script',$true)
+              ),
+                 $(($filePaths |
+                  & { process {
+                      $inObj = $_
+                      if ($inObj -is [Management.Automation.CommandInfo]) {
+                          $inObj
+                      }
+                      elseif ($inObj -is [IO.FileInfo] -and $inObj.Extension -eq '.ps1') {
+                          $ExecutionContext.SessionState.InvokeCommand.GetCommand($inObj.Fullname, 'ExternalScript')
+                      }
+                      elseif ($inObj -is [string] -or $inObj -is [Management.Automation.PathInfo]) {
+                          $resolvedPath = $ExecutionContext.SessionState.Path.GetResolvedPSPathFromPSPath($inObj)
+                          if ($resolvedPath) {
+                              $pathItem = Get-item -LiteralPath $resolvedPath
+                              if ($pathItem -is [IO.FileInfo] -and $pathItem.Extension -eq '.ps1') {
+                                  $ExecutionContext.SessionState.InvokeCommand.GetCommand($pathItem.FullName, 'ExternalScript')
+                              } else {                    
+                                  foreach ($pathItem in @(Get-ChildItem -LiteralPath $pathItem -File -Recurse)) {
+                                      if ($pathItem.Extension -eq '.ps1') {
+                                          $ExecutionContext.SessionState.InvokeCommand.GetCommand($pathItem.FullName, 'ExternalScript')
+                                      }
+                                  }
+                              }
+                          }            
+                      }
+                  } }
+              ))
+              # 'unroll' the collection by iterating over it once.
+              $filteredCollection = $inputCollection =
+                  @(foreach ($in in $inputCollection) {
+                      $in
+                  })
+              # Since filtering conditions have been passed, we must filter item-by-item
+              $filteredCollection = :nextItem foreach ($item in $inputCollection) {
+                  # we set $this, $psItem, and $_ for ease-of-use.
+                  $this = $_ = $psItem = $item
+               
+                  # Some of the items may be variables.
+                  if ($item -is [Management.Automation.PSVariable]) {
+                      # In this case, reassign them to their value.
+                      $this = $_ = $psItem = $item = $item.Value
+                  }
+                  
+                  # Some of the items may be enumerables
+                  $unrolledItems = 
+                      if ($item.GetEnumerator -and $item -isnot [string]) {
+                          @($item.GetEnumerator())
+                      } else {
+                          $item
+                      }
+                  foreach ($item in $unrolledItems) {
+                      $this = $_ = $psItem = $item
+                      if (-not $(
+                              $_.Name -match '^(?>demo|walkthru)\.ps1$' -or
+                              $_.Name -match '\.(?>demo|walkthru)\.ps1$'
+                          
+                      )) { continue } 
+                      $item
+                  }
+                  
+                  
+              }
+              # Walk over each item in the filtered collection
+              foreach ($item in $filteredCollection) {
+                  # we set $this, $psItem, and $_ for ease-of-use.
+                  $this = $_ = $psItem = $item
+              if ($item.value -and $item.value.pstypenames.insert) {
+                  if ($item.value.pstypenames -notcontains 'demofiles') {
+                      $item.value.pstypenames.insert(0, 'demofiles')
+                  }
+              }
+              elseif ($item.pstypenames.insert -and $item.pstypenames -notcontains 'demofiles') {
+                  $item.pstypenames.insert(0, 'demofiles')
+              }
+              $item
+                          
+              }   
+              ))
+        $currentDirectoryDemos = 
+            Get-ChildItem -Filter *.ps1 -Path $pwd |
+            Where-Object {
+                $_.Name -match '^(?>demo|walkthru)\.ps1$' -or
+                $_.Name -match '\.(?>demo|walkthru)\.ps1$'
+            }
+        
+        $allDemoFiles += @(
             $(
             # Collect all items into an input collection
             $inputCollection =$(
             $executionContext.SessionState.InvokeCommand.GetCommands('*','Script',$true)
             ),
-               $(($filePaths |
+               $(($currentDirectoryDemos |
                 & { process {
                     $inObj = $_
                     if ($inObj -is [Management.Automation.CommandInfo]) {
@@ -119,36 +208,6 @@ function Get-Demo {
                 @(foreach ($in in $inputCollection) {
                     $in
                 })
-            # Since filtering conditions have been passed, we must filter item-by-item
-            $filteredCollection = :nextItem foreach ($item in $inputCollection) {
-                # we set $this, $psItem, and $_ for ease-of-use.
-                $this = $_ = $psItem = $item
-             
-                # Some of the items may be variables.
-                if ($item -is [Management.Automation.PSVariable]) {
-                    # In this case, reassign them to their value.
-                    $this = $_ = $psItem = $item = $item.Value
-                }
-                
-                # Some of the items may be enumerables
-                $unrolledItems = 
-                    if ($item.GetEnumerator -and $item -isnot [string]) {
-                        @($item.GetEnumerator())
-                    } else {
-                        $item
-                    }
-                foreach ($item in $unrolledItems) {
-                    $this = $_ = $psItem = $item
-                    if (-not $(
-                            $_.Name -match '^(?>demo|walkthru)\.ps1$' -or
-                            $_.Name -match '\.(?>demo|walkthru)\.ps1$'
-                        
-                    )) { continue } 
-                    $item
-                }
-                
-                
-            }
             # Walk over each item in the filtered collection
             foreach ($item in $filteredCollection) {
                 # we set $this, $psItem, and $_ for ease-of-use.
@@ -165,6 +224,7 @@ function Get-Demo {
                         
             }   
             )
+        )
         $allDemoFiles |
             Where-Object Name -like "*$demoName*" |
             Import-Demo
