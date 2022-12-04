47faafbdecd0f8bcbad2a7728a86b33973bee538
James Brundage <@github.com>
2022-12-03 23:57:26 -0800

    Adding test for Export-Demo

--- ShowDemo.tests.ps1
+++ ShowDemo.tests.ps1
@@ -3,4 +3,11 @@ describe ShowDemo {
         $showedADemo = Show-Demo -NonInteractive 
         $showedADemo | Should -match '\e\[' # and it had an escape sequence for color
     }
+
+    it 'Can export a demo as markdown' {
+        $exportedDemo = Export-Demo -DemoPath .\demo.ps1 -OutputPath .\demo.md
+        $exportedDemo.Extension | Should -be '.md'
+        $exportedContent = Get-Content $exportedDemo.FullName -Raw
+        $exportedContent | Should -BeLike '*###*1.*'
+    }
 }
