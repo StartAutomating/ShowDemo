0162c19a5a28e1c543f87fad06323bff234f38b5
James Brundage <@github.com>
2022-12-03 22:00:56 -0800

    Adding workflow definitions (Fixes #8)

---
+++ ShowDemo.GitHubWorkflow.PSDevOps.ps1
@@ -0,0 +1,13 @@
+
+#requires -Module PSDevOps
+#requires -Module ShowDemo
+
+Import-BuildStep -Module ShowDemo
+
+Push-Location $PSScriptRoot
+
+New-GitHubWorkflow -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, BuildShowDemo -OutputPath @'
+.\.github\workflows\BuildShowDemo.yml
+'@ -Name "Build, Test, and Release ShowDemo" -On Push, PullRequest
+
+Pop-Location
