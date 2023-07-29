6a0b9abbffe016c56ef05c702daa1019207409e3
James Brundage <@github.com>
2023-07-29 16:20:22 -0700

    Paritioning Build Files (Fixes #48)

--- Build/ShowDemo.GitHubAction.PSDevOps.ps1
+++ Build/ShowDemo.GitHubAction.PSDevOps.ps1
@@ -1,6 +1,8 @@
 ﻿#requires -Module PSDevOps
 #requires -Module ShowDemo
+Push-Location ($PSScriptRoot | Split-Path)
 Import-BuildStep -ModuleName ShowDemo
 New-GitHubAction -Name "DemoPowerShell" -Description @'
 Make Demos of your PowerShell projects.
 '@ -Action DemoPowerShell -Icon terminal -OutputPath .\action.yml
+Pop-Location
