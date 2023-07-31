f96eacee270cae9234147fd2eeef063ab5826d6b
James Brundage <@github.com>
2023-07-30 22:26:13 -0700

    Import-Demo - Improving Inner docs (re #57)

--- Commands/Import-Demo.ps1
+++ Commands/Import-Demo.ps1
@@ -139,11 +139,16 @@
             }
         }
 
+        # If we have a named chapter
         if ($currentChapter) {
+            # add any remaining tokens to it.
             $currentChapter.Tokens = $chapterTokens
             $chapterTokens = @()
+            # and add the chapter.
             $chapters += $currentChapter
         } elseif ($chapterTokens) {
+            # Otherwise, if we have chapter tokens by no named chapter
+            # create an empty chapter and add the steps to it.
             $chapters += [Ordered]@{
                 Number = ''
                 Name = ''
@@ -152,6 +157,7 @@
             }
         }
 
+        # Create the demo object.
         $demoFile = [PSCustomObject][Ordered]@{
             PSTypeName = 'Demo'
             Name       = $demoName
@@ -159,16 +165,14 @@
             DemoScript = $DemoScript
         }
 
-
-
+        # And add each chapter to it
         $demoFile | 
             Add-Member NoteProperty Chapters @(
                 foreach ($chapter in $chapters) {
+                    # linking back to the demo as we go (#57).
                     [PSCustomObject]([Ordered]@{PSTypeName='Demo.Chapter';Demo=$demoFile} + $chapter)
                 }
-            ) -Force -PassThru
-
-        # $demoFile
+            ) -Force -PassThru # We -PassThru the modified object to return it.
     }
 }
 
