cecf2bd63f44d6d7a1d2f4e68a9d3b58db566d20
James Brundage <@github.com>
2023-09-30 11:48:21 -0700

    Adding Demo.Start event (Fixes #67)

--- Types/Demo/Start.ps1
+++ Types/Demo/Start.ps1
@@ -1,2 +1,10 @@
+<#
+.SYNOPSIS
+    Starts a Demo
+.DESCRIPTION
+    Starts a Demo file.
+#>
 $this | Add-Member NoteProperty Status Running -Force
 $this | Add-Member NoteProperty DemoStarted ([DateTime]::Now) -Force
+
+$null = New-Event -SourceIdentifier Demo.Start -Sender $this -EventArguments $args
