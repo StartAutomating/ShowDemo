4553e7e5ce22cc75e2e21fd648d83d9545b9482c
James Brundage <@github.com>
2023-09-30 11:56:47 -0700

    Adding Demo.NextStep event (Fixes #70)

--- Types/Demo/NextStep.ps1
+++ Types/Demo/NextStep.ps1
@@ -1,6 +1,14 @@
+<#
+.SYNOPSIS
+    Go to the next demo step
+.DESCRIPTION
+    Advances a demo to the next step.
+#>
 $demo = $this
 $demo.CurrentStep++
 # No more steps in this chapter
 if (-not $demo.CurrentChapter.Steps[$demo.CurrentStep - 1]) {
     $demo.NextChapter()
 }
+
+$null = New-Event -SourceIdentifier Demo.NextChapter -Sender $this -EventArguments $args
