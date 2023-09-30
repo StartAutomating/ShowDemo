42c4cd84707f44ed4da59388f648ad521c4c6bf6
James Brundage <@github.com>
2023-09-30 11:51:48 -0700

    Adding Demo.Stop event (Fixes #68)

--- Types/Demo/Stop.ps1
+++ Types/Demo/Stop.ps1
@@ -1,5 +1,13 @@
+<#
+.SYNOPSIS
+    Stops a demo
+.DESCRIPTION
+    Stops a demo that is currently running
+#>
 $this | Add-Member NoteProperty Status Stopped -Force
 $this | Add-Member NoteProperty StepToRun $null -Force
 $this | Add-Member NoteProperty DemoFinished ([datetime]::Now) -Force
 $demo | Add-Member NoteProperty CurrentChapter $null -Force
 $demo | Add-Member NoteProperty CurrentStep $null -Force
+
+$null = New-Event -SourceIdentifier Demo.Stop -Sender $this -EventArguments $args
