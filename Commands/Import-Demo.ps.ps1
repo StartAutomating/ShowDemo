bc23802014263178fb3727fae5c259fef0d2eb93
James Brundage <@github.com>
2023-10-07 15:45:34 -0700

    Import-Demo - Adjusting Validity Pattern (re #86)

--- Commands/Import-Demo.ps.ps1
+++ Commands/Import-Demo.ps.ps1
@@ -32,7 +32,7 @@
 
     begin {
         $ChapterExpression  = '^\s{0,}(?<cn>(?:\d+\.){1,})\s{0,}'
-        $ValidDemoFile      = '(?>(?<=^demo)|\.demo)\.(?>ps1|clixml)$'
+        $ValidDemoFile      = [regex]::new('\.demo\.(?>ps1|clixml)$','IgnoreCase')
         if (-not $script:CachedDemos) {
             $script:CachedDemos = [Ordered]@{}
         }
