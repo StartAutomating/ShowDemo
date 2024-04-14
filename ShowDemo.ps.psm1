9eedb4c5dbcadab6a486c1b209e9e0bb520dd9b1
James Brundage <@github.com>
2024-04-14 08:13:54 -0700

    feat: Exporting $ShowDemo ( Fixes #106 )

--- ShowDemo.ps.psm1
+++ ShowDemo.ps.psm1
@@ -1,1 +1,6 @@
 [Include('*-*.ps1')]$PSScriptRoot
+
+$thisModule = $MyInvocation.MyCommand.ScriptBlock.Module
+$ExecutionContext.SessionState.PSVariable.Set($thisModule.Name, $thisModule)
+
+Export-ModuleMember -Variable $thisModule.Name  -Function * -Alias *
