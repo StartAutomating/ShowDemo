6619242512d14c732b634a08459e317aa41c6e95
StartAutomating <StartAutomating@github.com>
2024-04-14 15:16:59 +0000

    feat: Mounting ShowDemo ( Fixes #107 )

--- ShowDemo.psm1
+++ ShowDemo.psm1
@@ -10,6 +10,16 @@
 }
 
 $thisModule = $MyInvocation.MyCommand.ScriptBlock.Module
+$thisModule.pstypenames.insert(0, $thisModule.Name)
 $ExecutionContext.SessionState.PSVariable.Set($thisModule.Name, $thisModule)
+
+$newDriveSplat = [ordered]@{
+    Name = $thisModule.Name
+    PSProvider = 'FileSystem'
+    Root = $PSScriptRoot
+    ErrorAction = 'Ignore'
+}
+
+New-PSDrive @newDriveSplat
 
 Export-ModuleMember -Variable $thisModule.Name  -Function * -Alias *
